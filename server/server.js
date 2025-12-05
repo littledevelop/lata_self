0c06a70880486e7fe1c3ac5271f464d692be4d45
Lata chapadia <122079514+littledevelop@users.noreply.github.com>
2025-12-05 10:29:59 +0530

    contact configuration changed

--- server/server.js
+++ server/server.js
@@ -65,10 +65,15 @@ app.post('/api/contact', async (req, res) => {
 
     // Send email
     const transporter = nodemailer.createTransport({
-      service: 'gmail',
-      auth: {
+        host: "smtp.gmail.com",
+        port: 587,
+        secure: false,
+        auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
+      },
+       tls: {
+        rejectUnauthorized: false
       },
     });
 
